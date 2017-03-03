4090e9b0b8074176da7ad8a637d9c9b2f17d1381
Stephen Burden <spburden@hotmail.com>
2017-03-03 15:58:31 -0800

    decompose scoreboard into seperate components

---
+++ README.md
@@ -0,0 +1,7 @@
+SCOREBOARD (1st React Application)
+Built Following the Treehouse Tutorials
+
+To run:
+In the teminal go to the project directory are enter the command:
+`python -m SimpleHTTPServer 8000`
+Then in the browser go to localhost:8000
--- app.jsx
+++ app.jsx
@@ -1,31 +1,77 @@
-function Application() {
+function Header(props) {
   return (
-    <div className="application">
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
 
-      <div className="header">
-        <h1>Scoreboard</h1>
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired,
+};
+
+function Counter(props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired,
+};
+
+function Player(props) {
+  return (
+    <div className="player">
+      <div className="player-name">
+        {props.name}
       </div>
+      <div className="player-score">
+        <Counter score={props.score} />
+      </div>
+    </div>
+  );
+}
+
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
+};
+
+Player.defaultProps = {
+  score: 0,
+};
+
+function Application(props) {
+  return (
+    <div className="scoreboard">
+
+      <Header title={props.title} />
 
       <div className="players">
-        <div className="player">
-          <div className="player-name">
-            Stephen
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action-decrement"> - </button>
-              <div className="counter-score"> 12 </div>
-              <button className="counter-action-increment"> + </button>
-            </div>
-          </div>
-        </div>
+        <Player name="Stephen" score={21} />
+        <Player name="Jason" score={16} />
       </div>
 
     </div>
   );
 }
 
-ReactDOM.render(<Application />, document.getElementById('container'));
+Application.propTypes = {
+
+};
+
+Application.defaultProps = {
+
+};
+
+ReactDOM.render(<Application title="Scoreboard"/>, document.getElementById('container'));
+
+
 // var PLAYERS = [
 //   {
 //     name: "Jim Hoskins",
