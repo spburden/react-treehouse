e1210eae5394568ba0e83f03d53229aedb6191bb
Stephen Burden <spburden@hotmail.com>
2017-03-03 14:21:44 -0800

    first steps for scoreboard

--- app.jsx
+++ app.jsx
@@ -1,4 +1,31 @@
-ReactDOM.render(<h1>WHAT UPP!!</h1>, document.getElementById('container'));
+function Application() {
+  return (
+    <div className="application">
+
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Stephen
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action-decrement"> - </button>
+              <div className="counter-score"> 12 </div>
+              <button className="counter-action-increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
 // var PLAYERS = [
 //   {
 //     name: "Jim Hoskins",
