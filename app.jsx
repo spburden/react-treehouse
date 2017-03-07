fe7680a8ff8669c4e728a2bdcb7799ae992934cf
Stephen Burden <spburden@hotmail.com>
2017-03-06 23:44:15 -0800

    add in/decrement functions to counter class

--- app.jsx
+++ app.jsx
@@ -38,12 +38,24 @@ var Counter = React.createClass({
     }
   },
 
+  incrementScore: function(e) {
+    this.setState ({
+      score: (this.state.score + 1),
+    });
+  },
+
+  decrementScore: function(e) {
+    this.setState ({
+      score: (this.state.score - 1),
+    });
+  },
+
   render: function() {
     return (
       <div className="counter">
-        <button className="counter-action decrement"> - </button>
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
         <div className="counter-score"> {this.state.score} </div>
-        <button className="counter-action increment"> + </button>
+      <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
     );
   }
